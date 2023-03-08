34778269c832787fcc7f67dffd65801cb38aeea7
CHAYEONIL <jeay123@naver.com>
2023-03-08 18:01:28 +0900

    [ADD] 회원탈퇴 API

---
+++ src/main/java/com/example/demo/src/user/model/PatchUserStatusReq.java
@@ -0,0 +1,15 @@
+package com.example.demo.src.user.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PatchUserStatusReq {
+    private int userId;
+    private String status;
+}
